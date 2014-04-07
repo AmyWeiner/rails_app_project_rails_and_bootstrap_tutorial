12699ccd04f114a7714226b7b089c8644e1f18ef
Amy Weiner <arweiner@alumni.stanford.edu>
2014-04-06 20:03:14 -0700

    Add a form to the home page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,21 @@
 class VisitorsController < ApplicationController
-
   def new
+    @visitor = Visitor.new
   end
 
-  def contact
+  def create
+    @visitor = Visitor.new(secure_params) 
+    if @visitor.valid?
+      flash.now[:notice] = "Chose #{@visitor.favorite}."
+      render :new 
+    else
+      render :new 
+    end
   end
 
+  private
+
+    def secure_params 
+      params.require(:visitor).permit(:favorite, :comment)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsBootstrap::Application.routes.draw do
+  resources :visitors, only: [:new, :create]
   root :to => 'visitors#new'
   get "/new_contact", to: "visitors#contact", as: "new_contact"
 end
